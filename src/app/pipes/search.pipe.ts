e717bfc2f4c19d159d0086cf2a09814986eecd75
riyakhandelwal <riya.khandelwal@moveinsync.com>
2020-02-18 01:14:58 +0530

    assignment submission

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -5,13 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform(value: any, args?: any, colName:any): any {
     if(!value) return null;
     if(!args) return value;
 
       args= args.toLowerCase();
       return value.filter(function(item){
-        return JSON.stringify(item).toLowerCase().includes(args);
+        return item[colName].toLowerCase().includes(args);
       });
   }
 
